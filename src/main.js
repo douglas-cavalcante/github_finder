ed4b1f9c8fb0d011e97cae87e4492a4d76fa8919
douglas-cavalcante <henrique.douglas.costaa@gmail.com>
2019-02-27 15:25:35 -0300

    Adiciona mini-projeto

---
+++ src/main.js
@@ -0,0 +1,96 @@
+import api from "./api";
+
+class App {
+    constructor() {
+
+        this.repositories = [];
+
+        this.formEl = document.getElementById("repo-form");
+        this.inputEl = document.querySelector('input[name=repository]')
+        this.listEl = document.getElementById("repo-list");
+
+        this.registerHandlers();
+    }
+
+    registerHandlers() {
+        this.formEl.onsubmit = (event) => this.addRespository(event);
+    }
+
+    async addRespository(event) {
+        event.preventDefault();
+        console.log("resultado")
+        const repoInput = this.inputEl.value;
+
+        if (repoInput.length === 0) return;
+        this.setLoading();
+        try {
+
+
+            const response = await api.get(`/repos/${repoInput}`);
+            console.log(response)
+
+            const { name, description, html_url, owner: { avatar_url } } = response.data;
+
+            this.repositories.push({
+                name,
+                description,
+                avatar_url,
+                html_url,
+            });
+            this.inputEl.value = '';
+            this.render();
+        } catch (error) {
+            alert("Não exite")
+        }
+        this.setLoading(false);
+    }
+
+    setLoading(loading = true) {
+        if(loading === true) {
+            let loadingEl = document.createElement('span');
+            loadingEl.appendChild(document.createTextNode("Carragando ..."))
+            loadingEl.setAttribute('id','loading');
+            this.formEl.appendChild(loadingEl);
+
+        }else {
+            document.getElementById('loading').remove();
+        }
+    }
+
+    render() {
+
+        //Limpa o array
+        this.listEl.innerHTML = "";
+
+
+        this.repositories.forEach(repo => {
+
+            console.log(repo)
+            let imgEl = document.createElement("img");
+            imgEl.setAttribute("src", repo.avatar_url);
+
+            let titleEl = document.createElement("strong");
+            titleEl.appendChild(document.createTextNode(repo.name));
+
+            let descriptionEL = document.createElement("p")
+            descriptionEL.appendChild(document.createTextNode(repo.description));
+
+            let linkEl = document.createElement("a");
+            linkEl.setAttribute("target", "_blank");
+            linkEl.setAttribute('href', repo.html_url)
+            linkEl.appendChild(document.createTextNode("Acessar"));
+
+            let listItemEl = document.createElement("li");
+
+            listItemEl.appendChild(imgEl);
+            listItemEl.appendChild(titleEl);
+            listItemEl.appendChild(descriptionEL);
+            listItemEl.appendChild(linkEl);
+
+            this.listEl.appendChild(listItemEl);
+
+        });
+    }
+}
+
+const MeuApp = new App();
